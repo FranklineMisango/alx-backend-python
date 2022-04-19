9d1791554cff18ca26bf852cbe6523ca1f2bfe13
Virgo Alpha <bensonkingori96@gmail.com>
2022-04-19 17:35:49 +0400

    s

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""Contains a coroutine that takes no arguments, loops 10 times awaiting 1 sec each then yield a random no."""
+"""Takes no arguments, loops 10 times awaiting 1 sec each then yield a random no."""
 
 import asyncio
 import random
